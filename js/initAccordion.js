5172708bd6b75cf2b6a806850a521f5c6c500977
Dima163163 <khlopovdima@gmail.com>
2025-02-10 13:40:58 +0400

    fix js

--- js/initAccordion.js
+++ js/initAccordion.js
@@ -1,7 +1,6 @@
 export const initAccordion = () => {
   const accordionItemsList = document.querySelector('.why-choose-us_list');
   const accordionElements = document.querySelectorAll('.why-choose-us_item');
-  const accoridonBtn = document.querySelectorAll('.why-choose-us_item_top');
 
   accordionItemsList.addEventListener('click', (e) => {
     const target = e.target;
